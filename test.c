b51c3e53170cbaebcde2fd36e1a61b568932cb4a
gdezan <33992304+gdezan@users.noreply.github.com>
2018-06-12 22:10:11 -0300

    more work on tests

--- test.c
+++ test.c
@@ -49,13 +49,13 @@ typedef struct matrix {
 
 
 typedef struct {
-  int *array;
+  float *array;
   size_t used;
   size_t size;
 } Array;
 
 void initArray(Array *a, size_t initialSize) {
-  a->array = (int *)malloc(initialSize * sizeof(int));
+  a->array = (float *)malloc(initialSize * sizeof(float));
   a->used = 0;
   a->size = initialSize;
 }
@@ -63,7 +63,7 @@ void initArray(Array *a, size_t initialSize) {
 void insertArray(Array *a, float element[3]) {
   if (a->used == a->size) {
     a->size *= 2;
-    a->array = (int *)realloc(a->array, a->size * sizeof(int));
+    a->array = (float *)realloc(a->array, a->size * sizeof(float));
   }
   a->array[a->used++] = element[3];
 }
@@ -103,36 +103,33 @@ int setValue(matrix m, Array a){
     return 0;
 }
 
-// int checkValue(matrix *m){
-//     if (a){
-//         printf("\nDigite a linha do valor: ");
-//         int row = intInput();
-//         printf("\nDigite a coluna do valor: ");
-//         int col = intInput();
+int checkValue(matrix m, Array a){
 
-//         if (row < 0 || row >= m -> rows || col < 0 || col >= m -> columns){
-//             printf("\nValor: 0");
-//             printf("\n\nPressione ENTER para continuar ");
-//             getchar();
-//             return 0;
-//         }
+        printf("\nDigite a linha do valor: ");
+        int row = intInput();
+        printf("\nDigite a coluna do valor: ");
+        int col = intInput();
 
-//         node *current = m -> a[row];
-//         while (current){
-//             if (current -> column == col){
-//                 printf("\nValor: %.3f", current -> value);
-//                 printf("\n\nPressione ENTER para continuar ");
-//                 getchar();
-//                 return 0;
-//             }
-//             current = current -> next;
-//         }
-//     } else {
-//         printf("\nMatriz nao inicializada");
-//         printf("\n\nPressione ENTER para continuar ");
-//         getchar();
-//     }
-// }
+        if (row < 0 || row >= m.rows || col < 0 || col >= m.columns){
+            printf("\nEsse valor esta fora do escopo da matriz");
+            printf("\n\nPressione ENTER para continuar ");
+            getchar();
+            return 0;
+        }
+        int found = 0;
+        for (int i = 0; i < a.used; i++){
+            if (a.array[0] == row && a.array[1] == col){
+                printf("\nValor: %.3f", a.array[2]);
+                found = 1;
+            }
+        }
+        if (found == 0){
+            printf("\nValor: 0");
+        }
+        printf("\n\nPressione ENTER para continuar ");
+        getchar();
+        return 0;
+}
 
 
 int main() {
@@ -155,11 +152,12 @@ int main() {
             case 2:
                 setValue(m, a);
                 break;
-//             case 3:
-//                 checkValue(mat);
-//                 break;
+            case 3:
+                checkValue(m, a);
+                break;
             case 0:
                 return 0;
+                freeArray(&a);
             default:
                 printf("Essa opcao nao existe.\n");
                 printf("\n\nPressione ENTER para continuar ");
@@ -167,6 +165,7 @@ int main() {
                 break;
         }
 
-        fflush(stdin);
+        
     }
+    
 }
